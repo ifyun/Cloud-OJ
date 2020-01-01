9e048bc28817792cb06608184ee13c4a23dcf822
Cloud <wfloatingcloud@outlook.com>
2020-01-01 14:36:37 +0800

    Initial Commit.

---
+++ register-center/src/main/java/top/cloudli/registercenter/RegisterCenterApplication.java
@@ -0,0 +1,18 @@
+package top.cloudli.registercenter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@Slf4j
+@SpringBootApplication
+@EnableEurekaServer
+public class RegisterCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterCenterApplication.class, args);
+        log.info("Cloud OJ 服务注册中心启动完成.");
+    }
+
+}
---
+++ register-center/src/test/java/top/cloudli/registercenter/RegisterCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package top.cloudli.registercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
