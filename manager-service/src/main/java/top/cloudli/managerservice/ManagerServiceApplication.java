dc412cd46e8b95b4860d36d2ceead9174775a942
Cloud <wfloatingcloud@outlook.com>
2020-01-04 15:13:04 +0800

    修复无法注册问题.

---
+++ manager-service/src/main/java/top/cloudli/managerservice/ManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package top.cloudli.managerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerServiceApplication.class, args);
+    }
+
+}
---
+++ manager-service/src/test/java/top/cloudli/managerservice/ManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package top.cloudli.managerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
