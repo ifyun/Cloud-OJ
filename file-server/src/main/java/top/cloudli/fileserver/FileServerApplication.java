b519902315cd91d9c9f1639a3aa06e6c5294fdfc
Cloud <wfloatingcloud@outlook.com>
2020-01-04 11:07:47 +0800

    Initial Commit.

---
+++ file-server/src/main/java/top/cloudli/fileserver/FileServerApplication.java
@@ -0,0 +1,13 @@
+package top.cloudli.fileserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileServerApplication.class, args);
+    }
+
+}
