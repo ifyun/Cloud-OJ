b4c79d1216358eaf10e8dc2a9c6a19ab812e445e
Cloud <wfloatingcloud@outlook.com>
2020-01-17 18:11:40 +0800

    增加了测试数据上传和删除.

--- file-server/src/main/java/top/cloudli/fileserver/controller/TestDataController.java
+++ file-server/src/main/java/top/cloudli/fileserver/controller/TestDataController.java
@@ -2,12 +2,15 @@ package top.cloudli.fileserver.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import top.cloudli.fileserver.model.Result;
+import top.cloudli.fileserver.model.TestData;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 测试数据文件上传与删除
@@ -20,17 +23,45 @@ public class TestDataController {
     @Value("${project.test-data-dir}")
     private String testDataDir;
 
+    @GetMapping("{problemId}")
+    public ResponseEntity<?> getTestData(@PathVariable int problemId) {
+        File dir = new File(testDataDir + problemId);
+        File[] files = dir.listFiles();
+
+        List<TestData> testDataList = new ArrayList<>();
+        if (files != null)
+            for (File file : files) {
+                TestData testData = new TestData();
+                // 读取文件名称
+                testData.setFileName(file.getName());
+                testData.setSize(file.length());
+
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+                    // 读取文件内容
+                    testData.setContent(reader.lines().collect(Collectors.joining("\n")));
+                } catch (IOException ignored) {
+
+                }
+
+                testDataList.add(testData);
+            }
+
+        return testDataList.size() > 0 ?
+                ResponseEntity.ok(testDataList) :
+                ResponseEntity.noContent().build();
+    }
+
     @PostMapping("")
-    public Object uploadTestData(@RequestParam int problemId, @RequestParam MultipartFile[] files) {
+    public ResponseEntity<?> uploadTestData(@RequestParam int problemId, @RequestParam("file") MultipartFile[] files) {
         if (files.length == 0)
-            return new Result(204, "未选择文件.");
+            return ResponseEntity.badRequest().body("未选择文件.");
 
         // 根据题目 id 创建目录
         File dir = new File(testDataDir + problemId);
 
         if (!dir.exists()) {
             if (!dir.mkdir())
-                return new Result(500, "无法创建目录.");
+                return ResponseEntity.status(500).body("无法创建目录.");
         }
 
         for (MultipartFile file : files) {
@@ -41,22 +72,22 @@ public class TestDataController {
                 file.transferTo(dest);
                 log.info("上传文件 {} 到 {}", fileName, testDataDir);
             } catch (IOException e) {
-                return new Result(500, e.getMessage());
+                return ResponseEntity.status(500).body(e.getMessage());
             }
         }
 
-        return new Result(200, "上传成功.");
+        return ResponseEntity.ok("{\"msg\": \"上传成功\"}");
     }
 
-    @DeleteMapping("")
-    public Object deleteTestData(String name) {
-        File dest = new File(testDataDir + name);
+    @DeleteMapping("{problemId}")
+    public ResponseEntity<?> deleteTestData(@PathVariable int problemId, String name) {
+        File dest = new File(testDataDir + problemId + "/" + name);
 
         if (dest.delete()) {
-            log.info("删除文件 {}", name);
-            return new Result(200, "删除成功.");
+            log.info("删除文件 {}/{}", problemId, name);
+            return ResponseEntity.noContent().build();
         }
 
-        return new Result(500, "删除失败.");
+        return ResponseEntity.status(500).body("删除文件失败");
     }
 }
--- file-server/src/main/java/top/cloudli/fileserver/model/TestData.java
+++ file-server/src/main/java/top/cloudli/fileserver/model/TestData.java
@@ -7,7 +7,8 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Result {
-    private int code;
-    private String info;
+public class TestData {
+    private String fileName;
+    private String content;
+    private long size;
 }
