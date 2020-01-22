627e06bd93ba9e9ebbb5fa818efdfe2d1826f209
Cloud <wfloatingcloud@outlook.com>
2020-01-22 16:15:14 +0800

    添加了后台的竞赛/作业管理部分.

--- OJ-Database.sql
+++ OJ-Database.sql
@@ -1,3 +1,13 @@
+create table contest
+(
+    contest_id   int auto_increment
+        primary key,
+    contest_name varchar(64) null,
+    start_at     timestamp   not null comment '开始时间',
+    end_at       timestamp   not null comment '结束时间'
+)
+    comment '竞赛和作业';
+
 create table problem
 (
     problem_id    int auto_increment
@@ -15,6 +25,18 @@ create table problem
     create_at     datetime   default CURRENT_TIMESTAMP not null comment '创建时间'
 )
     comment '题目表';
+
+create table `contest-problem`
+(
+    contest_id int null,
+    problem_id int null,
+    constraint `contest-problem_contest_contest_id_fk`
+        foreign key (contest_id) references contest (contest_id)
+            on update cascade on delete cascade,
+    constraint `contest-problem_problem_problem_id_fk`
+        foreign key (problem_id) references problem (problem_id)
+            on update cascade on delete cascade
+);
 
 create index problem_title_index
     on problem (title);
@@ -45,12 +67,16 @@ create table solution
     solution_id int auto_increment
         primary key,
     problem_id  int                                null,
+    contest_id  int                                null,
     language    int                                not null,
     state       int      default 2                 not null comment '状态(2->已提交,1->在判题队列,0->已判题)',
     result      int                                null comment '结果(4->编译错误,3->答案错误,2->部分通过,1->时间超限,0->完全正确)',
     pass_rate   double   default 0                 not null comment '通过率',
     user_id     varchar(32)                        null,
     submit_time datetime default CURRENT_TIMESTAMP not null comment '提交时间',
+    constraint solution_contest_contest_id_fk
+        foreign key (contest_id) references contest (contest_id)
+            on update cascade,
     constraint solution_problem_problem_id_fk
         foreign key (problem_id) references problem (problem_id)
             on update cascade on delete cascade,
@@ -104,6 +130,50 @@ create index user_name_index
 
 create index user_section_index
     on user (section);
+
+create definer = root@`%` view contest_problem as
+select `c`.`contest_id`    AS `contest_id`,
+       `c`.`contest_name`  AS `contest_name`,
+       `c`.`start_at`      AS `start_at`,
+       `c`.`end_at`        AS `end_at`,
+       `p`.`problem_id`    AS `problem_id`,
+       `p`.`title`         AS `title`,
+       `p`.`description`   AS `description`,
+       `p`.`input`         AS `input`,
+       `p`.`output`        AS `output`,
+       `p`.`sample_input`  AS `sample_input`,
+       `p`.`sample_output` AS `sample_output`,
+       `p`.`timeout`       AS `timeout`,
+       `p`.`score`         AS `score`,
+       `p`.`enable`        AS `enable`,
+       `p`.`category`      AS `category`,
+       `p`.`create_at`     AS `create_at`
+from ((`cloud_oj`.`contest-problem` `cp` join `cloud_oj`.`problem` `p` on ((`cp`.`problem_id` = `p`.`problem_id`)))
+         join `cloud_oj`.`contest` `c` on ((`cp`.`contest_id` = `c`.`contest_id`)));
+
+-- comment on column contest_problem.start_at not supported: 开始时间
+
+-- comment on column contest_problem.end_at not supported: 结束时间
+
+-- comment on column contest_problem.description not supported: 题目描述
+
+-- comment on column contest_problem.input not supported: 输入说明
+
+-- comment on column contest_problem.output not supported: 输出说明
+
+-- comment on column contest_problem.sample_input not supported: 示例输入
+
+-- comment on column contest_problem.sample_output not supported: 示例输出
+
+-- comment on column contest_problem.timeout not supported: 时间限制
+
+-- comment on column contest_problem.score not supported: 分数
+
+-- comment on column contest_problem.enable not supported: 是否可用
+
+-- comment on column contest_problem.category not supported: 分类
+
+-- comment on column contest_problem.create_at not supported: 创建时间
 
 create definer = root@`%` view judge_result as
 select `s`.`solution_id`               AS `solution_id`,
