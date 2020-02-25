3effc3311accc9840ffb9661a78cc1b8ca2e36d9
Cloud <wfloatingcloud@outlook.com>
2020-02-25 16:38:47 +0800

    修复搜索Bug，前端标签配色调整.

--- manager-service/src/main/java/top/cloudli/managerservice/controller/ProblemManagerController.java
+++ manager-service/src/main/java/top/cloudli/managerservice/controller/ProblemManagerController.java
@@ -37,7 +37,7 @@ public class ProblemManagerController {
     @GetMapping("pro")
     public ResponseEntity<?> getAll(String userId, String keyword, int page, int limit) {
         if (keyword != null)
-            return managerService.searchProblems(userId, keyword, (page - 1) * limit, limit, false);
+            return managerService.searchProblems(userId, keyword, (page - 1) * limit, limit, true);
         return managerService.getProblems(userId, (page - 1) * limit, limit);
     }
 
