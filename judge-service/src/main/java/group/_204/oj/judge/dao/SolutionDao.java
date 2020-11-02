b02bb44835c7eb2c7f573766501e11996b6ade4b
cloud <wfloatingcloud@outlook.com>
2020-11-02 12:49:17 +0800

    feat: show judge result on dialog

--- judge-service/src/main/java/group/_204/oj/judge/dao/SolutionDao.java
+++ judge-service/src/main/java/group/_204/oj/judge/dao/SolutionDao.java
@@ -4,13 +4,9 @@ import org.apache.ibatis.annotations.Mapper;
 import group._204.oj.judge.model.JudgeResult;
 import group._204.oj.judge.model.Solution;
 
-import java.util.List;
-
 @Mapper
 public interface SolutionDao {
-    List<Solution> getSubmitted();  // 获取已提交的答案
-
-    JudgeResult getResultBySolutionId(String solutionId);        // 获取单条 solution
+    JudgeResult getResultBySolutionId(String solutionId);
 
     int add(Solution solution);
 
--- judge-service/src/main/java/group/_204/oj/judge/dao/TaskDao.java
+++
@@ -1,9 +0,0 @@
-package group._204.oj.judge.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TaskDao {
-    String getUUID(String taskName);
-    void setUUID(String taskName, String uuid);
-}
