963b30f118b9bd7e60af5508d0ad8a7f071d3714
Cloud Li <wfloatingcloud@outlook.com>
2020-11-06 21:20:42 +0800

    Update settings page.

--- web/src/components/manage/Settings.vue
+++ web/src/components/manage/Settings.vue
@@ -3,7 +3,12 @@
     <el-card>
       <h3>系统信息</h3>
       <el-card style="margin-bottom: 35px">
-        <div><b>RabbitMQ 消息队列：</b></div>
+        <div><b>RabbitMQ 消息队列：</b>
+          <el-button style="float: right" icon="el-icon-refresh" size="mini"
+                     @click="getQueueInfo">
+            刷新
+          </el-button>
+        </div>
         <div style="margin-top: 20px">
           <el-tag type="warning" effect="dark">
             {{ queueInfo.inCommitQueue }}&nbsp;个提交等待写入
