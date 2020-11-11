b33ec9ef3751fc82f7dec2583a998d6ed4a6048f
Cloud Li <wfloatingcloud@outlook.com>
2020-11-11 22:19:11 +0800

    Update proxy config.

--- web/vue.config.js
+++ web/vue.config.js
@@ -6,6 +6,18 @@ module.exports = {
             '/api': {
                 target: 'http://localhost:8080',
                 changeOrigin: true
+            },
+            '/swagger-ui': {
+                target: 'http://localhost:8080',
+                changeOrigin: true
+            },
+            '/v2': {
+                target: 'http://localhost:8080',
+                changeOrigin: true
+            },
+            '/swagger-resources': {
+                target: 'http://localhost:8080',
+                changeOrigin: true
             }
         }
     }
