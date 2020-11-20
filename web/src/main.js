61912b1edc86bb5aa190fd4643046a5d0570baa8
Cloud Li <wfloatingcloud@outlook.com>
2020-11-20 16:03:20 +0800

    Update code style.

--- web/src/main.js
+++ web/src/main.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
+import Vue from "vue"
 import App from "@/App"
 import router from "@/router/router"
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import axios from 'axios'
-import qs from 'qs'
-import md5 from 'js-md5'
+import ElementUI from "element-ui"
+import "element-ui/lib/theme-chalk/index.css"
+import axios from "axios"
+import qs from "qs"
+import md5 from "js-md5"
 
 Vue.prototype.$axios = axios
 Vue.prototype.qs = qs
--- web/src/router/router.js
+++ web/src/router/router.js
@@ -1,35 +1,35 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue"
+import VueRouter from "vue-router"
 
 Vue.use(VueRouter)
-const Page404 = () => import('@/page/404')
-const Login = () => import('@/page/Login')
-const Problems = () => import('@/page/Problems')
-const Contest = () => import('@/page/Contest')
-const History = () => import('@/page/CommitHistory')
-const Profile = () => import('@/page/Profile')
-const Ranking = () => import('@/page/Ranking')
-const Manage = () => import('@/page/Manage')
-const Commit = () => import('@/page/Commit')
-const Help = () => import('@/page/Help')
+const Page404 = () => import("@/page/404")
+const Login = () => import("@/page/Login")
+const Problems = () => import("@/page/Problems")
+const Contest = () => import("@/page/Contest")
+const History = () => import("@/page/CommitHistory")
+const Profile = () => import("@/page/Profile")
+const Ranking = () => import("@/page/Ranking")
+const Manage = () => import("@/page/Manage")
+const Commit = () => import("@/page/Commit")
+const Help = () => import("@/page/Help")
 
 const routes = [
-    {path: '*', component: Page404},
-    {path: '/login', component: Login},
-    {path: '/', redirect: 'problems'},
-    {path: '/problems', component: Problems},
-    {path: '/contest', component: Contest},
-    {path: '/history', component: History},
-    {path: '/profile', component: Profile},
-    {path: '/ranking', component: Ranking},
-    {path: '/manage', component: Manage},
-    {path: '/commit', component: Commit},
-    {path: '/help', component: Help}
+    {path: "*", component: Page404},
+    {path: "/login", component: Login},
+    {path: "/", redirect: 'problems'},
+    {path: "/problems", component: Problems},
+    {path: "/contest", component: Contest},
+    {path: "/history", component: History},
+    {path: "/profile", component: Profile},
+    {path: "/ranking", component: Ranking},
+    {path: "/manage", component: Manage},
+    {path: "/commit", component: Commit},
+    {path: "/help", component: Help}
 ]
 
 const router = new VueRouter({
     routes,
-    mode: 'history'
+    mode: "history"
 })
 
 export default router
