9db267fb7836ebe76bb00131bdabbd2554de844b
Cloud Li <wfloatingcloud@outlook.com>
2020-11-21 15:35:16 +0800

    refactor: rename modules

---
+++ file-service/src/main/java/group/_204/oj/fileserver/FileServiceApp.java
@@ -0,0 +1,13 @@
+package group._204.oj.fileserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileServiceApp.class, args);
+    }
+
+}
---
+++ gateway/src/main/java/group/_204/oj/gateway/GatewayApp.java
@@ -0,0 +1,15 @@
+package group._204.oj.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApp.class, args);
+    }
+
+}
