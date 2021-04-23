b403ba60b1118d8319d58721d8c81d62a3d7bdbf
Cloud Li <wfloatingcloud@outlook.com>
2021-04-23 11:18:35 +0800

    Updated rabbitmq config

--- judge-service/src/main/java/group/_204/oj/judge/config/RabbitConfig.java
+++ judge-service/src/main/java/group/_204/oj/judge/config/RabbitConfig.java
@@ -1,12 +1,8 @@
 package group._204.oj.judge.config;
 
 import org.springframework.amqp.core.Queue;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class RabbitConfig {
@@ -14,15 +10,9 @@ public class RabbitConfig {
     public static final String JUDGE_QUEUE = "JudgeQueue";
     public static final String COMMIT_QUEUE = "CommitQueue";
 
-    @Value("${commit-queue-length:4000}")
-    private Integer commitQueueLength;
-
     @Bean
     public Queue commitQueue() {
-        Map<String, Object> args = new HashMap<>();
-        args.put("x-max-length", commitQueueLength);
-        args.put("x-overflow", "reject-publish");
-        return new Queue(COMMIT_QUEUE, true, false, false, args);
+        return new Queue(COMMIT_QUEUE);
     }
 
     @Bean
