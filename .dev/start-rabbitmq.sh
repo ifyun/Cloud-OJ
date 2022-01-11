e65e2b0e41f48d65a7cd29d1229e6051d0af5e0f
Cloud Li <wfloatingcloud@outlook.com>
2022-01-11 13:22:38 +0800

    build: change dev files

--- .dev/start-rabbitmq.sh
+++ .dev/start-rabbitmq.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-docker run --name rabbitmq_cloudoj \
-  -p 5672:5672 \
-  -p 15672:15672 \
-  -e "RABBITMQ_DEFAULT_USER=admin" \
-  -e "RABBITMQ_DEFAULT_PASS=admin" \
-  -v rabbitmq-cloudoj:/var/lib/rabbitmq/mnesia \
-  -d rabbitmq:3.8.2-management
+NAME=rabbitmq_cloudoj
+
+if docker ps -a | grep $NAME >/dev/null; then
+  docker start $NAME
+else
+  docker run --name $NAME \
+    -p 5672:5672 \
+    -p 15672:15672 \
+    -e "RABBITMQ_DEFAULT_USER=admin" \
+    -e "RABBITMQ_DEFAULT_PASS=admin" \
+    -v rabbitmq-cloudoj:/var/lib/rabbitmq/mnesia \
+    -d rabbitmq:3.8.2-management
+fi
