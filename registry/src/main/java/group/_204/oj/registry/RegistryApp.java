a37fbe7da061e11ef163926b4fc87dd817af0d88
Cloud Li <wfloatingcloud@outlook.com>
2022-01-11 13:11:31 +0800

    refactor: merge spring-boot-admin to registry

--- registry/src/main/java/group/_204/oj/registry/RegistryApp.java
+++ registry/src/main/java/group/_204/oj/registry/RegistryApp.java
@@ -1,13 +1,13 @@
 package group._204.oj.registry;
 
-import lombok.extern.slf4j.Slf4j;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@Slf4j
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAdminServer
 public class RegistryApp {
 
     public static void main(String[] args) {
