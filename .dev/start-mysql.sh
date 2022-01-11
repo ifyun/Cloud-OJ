947e572f2b2f58a0004f4499c5c4a35e8f96a509
Cloud Li <wfloatingcloud@outlook.com>
2022-01-11 13:22:38 +0800

    build: change dev files

--- .dev/start-mysql.sh
+++ .dev/start-mysql.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-docker run --name mysql_cloudoj \
-  -p 3306:3306 \
-  -e "TZ=Asia/Shanghai" \
-  -e "MYSQL_ROOT_PASSWORD=root" \
-  -e "MYSQL_ROOT_HOST=%" \
-  -v "$PWD"/sql:/docker-entrypoint-initdb.d \
-  -v mysql-cloudoj:/var/lib/mysql \
-  -d mysql:8.0
+NAME=mysql_cloudoj
+
+if docker ps -a | grep $NAME >/dev/null; then
+  docker start $NAME
+else
+  docker run --name $NAME \
+    -p 3306:3306 \
+    -e "TZ=Asia/Shanghai" \
+    -e "MYSQL_ROOT_PASSWORD=root" \
+    -e "MYSQL_ROOT_HOST=%" \
+    -v "$PWD"/sql:/docker-entrypoint-initdb.d \
+    -v mysql-cloudoj:/var/lib/mysql \
+    -d mysql:8.0
+fi
