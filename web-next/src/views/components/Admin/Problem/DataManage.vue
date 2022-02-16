69fa4292462f063707e5799312fe3e9f981d516e
Cloud Li <wfloatingcloud@outlook.com>
2022-02-16 14:55:51 +0800

    feat: complete data manage page

--- web-next/src/views/components/Admin/Problem/DataManage.vue
+++ web-next/src/views/components/Admin/Problem/DataManage.vue
@@ -4,17 +4,24 @@
             content-style="height: 100%; display: flex; flex-direction: column">
       <n-page-header :subtitle="subtitle" @back="back">
         <template #title>{{ title }}</template>
-        <template #extra>
-          <n-button type="primary" round>
-            <template #icon>
-              <n-icon>
-                <upload-icon/>
-              </n-icon>
-            </template>
-            上传文件
-          </n-button>
-        </template>
-        <n-data-table :loading="loading" :columns="tableColumns" :data="testData"/>
+        <n-space vertical size="large">
+          <n-data-table max-height="350" :loading="loading" :columns="columns" :data="testData"/>
+          <n-alert type="warning" title="注意换行符">
+            文件中的换行符必须为 LF，不能使用 CRLF。
+          </n-alert>
+          <n-upload action="/api/file/test_data" multiple :headers="headers" :data="uploadData" accept=".in,.out"
+                    :disabled="disableUpload" @beforeUpload="beforeUpload" @finish="handleUploadFinish">
+            <n-upload-dragger style="width: 100%">
+              <div>
+                <n-icon size="48" depth="3">
+                  <archive-icon/>
+                </n-icon>
+              </div>
+              <n-text style="font-size: 16px">点击或拖动文件到该区域上传</n-text>
+              <n-p depth="3">文件类型为 .in 和 .out，每个 .in 文件对应一个 .out 文件</n-p>
+            </n-upload-dragger>
+          </n-upload>
+        </n-space>
       </n-page-header>
     </n-card>
   </div>
@@ -25,34 +32,49 @@ import {useStore} from "vuex"
 import {useRoute, useRouter} from "vue-router"
 import {Options, Vue} from "vue-class-component"
 import {
+  NAlert,
   NBreadcrumb,
   NBreadcrumbItem,
   NButton,
   NCard,
   NDataTable,
   NIcon,
+  NP,
   NPageHeader,
+  NPopconfirm,
   NSpace,
+  NText,
+  NUpload,
+  NUploadDragger,
   useMessage
 } from "naive-ui"
 import {
+  ArchiveRound as ArchiveIcon,
   DeleteForeverRound as DeleteIcon,
-  FileDownloadOutlined as DownloadIcon,
-  UploadFileRound as UploadIcon
+  FileDownloadOutlined as DownloadIcon
 } from "@vicons/material"
+import axios from "axios"
 import Mutations from "@/store/mutations"
 import {ProblemApi} from "@/api/request"
-import {ErrorMsg, TestData} from "@/api/type"
+import {ErrorMsg, Problem, TestData} from "@/api/type"
+
+let self: any
 
 @Options({
   name: "DataManage",
   components: {
     NCard,
+    NSpace,
     NButton,
-    NIcon,
     NPageHeader,
     NDataTable,
-    UploadIcon
+    NUpload,
+    NUploadDragger,
+    NAlert,
+    NText,
+    NP,
+    NIcon,
+    ArchiveIcon
   }
 })
 export default class DataManage extends Vue {
@@ -63,54 +85,99 @@ export default class DataManage extends Vue {
 
   private loading: boolean = false
   private title = "测试数据管理"
-  private subtitle: string = ""
+  private problem: Problem | null = null
   private testData: Array<TestData> = []
 
-  private tableColumns = [
+  private columns = [
     {
       title: "#",
       align: "right",
       width: 50,
-      render: (row: TestData, rowIndex: number) => (<span>{rowIndex + 1}</span>)
+      render: (row: TestData, rowIndex: number) => <span>{rowIndex + 1}</span>
     },
     {
       title: "文件名",
       align: "left",
-      render: (row: TestData) => (<span>{row.fileName}</span>)
+      render: (row: TestData) => {
+        let type = ""
+        if (row.fileName.endsWith(".in")) {
+          type = "info"
+        } else if (row.fileName.endsWith(".out")) {
+          type = "success"
+        }
+        return <NText strong={true} type={type}>{row.fileName}</NText>
+      }
     },
     {
       title: "文件长度",
       align: "right",
-      render: (row: TestData) => (<span>{row.size} 字节</span>)
+      render: (row: TestData) => <span>{row.size} 字节</span>
     },
     {
       title: "操作",
       align: "center",
       width: 240,
-      render: () => (
+      render: (row: TestData) =>
           <NSpace size="small" justify="center">
-            <NButton size="small" type="primary" secondary={true}>
+            <NButton size="small" type="primary" secondary={true} onClick={() => self.downloadFile(row.fileName)}>
               {{
                 icon: () => <NIcon><DownloadIcon/></NIcon>,
                 default: () => <span>下载</span>
               }}
             </NButton>
-            <NButton size="small" type="error" secondary={true}>
+            <NPopconfirm>
               {{
-                icon: () => <NIcon><DeleteIcon/></NIcon>,
-                default: () => <span>删除</span>
+                trigger: () =>
+                    <NButton size="small" type="error" secondary={true}>
+                      {{
+                        icon: () => <NIcon><DeleteIcon/></NIcon>,
+                        default: () => <span>删除</span>
+                      }}
+                    </NButton>,
+                action: () => <NButton size="small" type="error" ghost={true}
+                                       onClick={() => self.deleteFile(row.fileName)}>确认</NButton>,
+                default: () => <span>确定要删除吗？</span>
               }}
-            </NButton>
+            </NPopconfirm>
           </NSpace>
-      )
     }
   ]
+
+  get subtitle() {
+    if (this.problem == null) {
+      return ""
+    } else {
+      return `${this.problem.problemId} - ${this.problem.title}`
+    }
+  }
 
   get userInfo() {
     return this.store.state.userInfo
   }
 
+  get headers() {
+    return {
+      "userId": this.userInfo.userId,
+      "token": this.userInfo.token
+    }
+  }
+
+  get uploadData() {
+    if (this.problem == null) {
+      return {}
+    } else {
+      return {
+        "problemId": this.problem?.problemId
+      }
+    }
+  }
+
+  get disableUpload(): boolean {
+    return this.loading || this.problem == null
+  }
+
   beforeMount() {
+    self = this
     const vNode =
         (<NBreadcrumb>
           <NBreadcrumbItem>题目管理</NBreadcrumbItem>
@@ -128,8 +195,8 @@ export default class DataManage extends Vue {
           problemId,
           this.userInfo
       ).then(p => {
-        this.subtitle = `${id} - ${p.title}`
-        this.queryData(problemId)
+        this.problem = p
+        this.queryData(p.problemId!)
       }).catch((error: ErrorMsg) => {
         this.message.error(error.toString())
         this.loading = false
@@ -153,15 +220,63 @@ export default class DataManage extends Vue {
   back() {
     this.router.back()
   }
+
+  beforeUpload({file}: any) {
+    const fileName = file.name as string
+    return fileName.endsWith(".in") || fileName.endsWith(".out")
+  }
+
+  handleUploadFinish({file}: any) {
+    this.message.success(`${file.name} 已上传`)
+    this.queryData(this.problem!.problemId!)
+  }
+
+  downloadFile(fileName: string) {
+    const url = `/api/file/test_data/${this.problem!.problemId!}/${fileName}`
+    const anchor = document.createElement("a")
+    axios.get(url, {headers: this.headers, responseType: "blob"})
+        .then(res => {
+          const objectUrl = window.URL.createObjectURL(res.data)
+          anchor.href = objectUrl
+          anchor.download = fileName
+          anchor.click()
+          window.URL.revokeObjectURL(objectUrl)
+        })
+        .catch((error) => {
+          this.message.error(error.toString())
+        })
+  }
+
+  deleteFile(fileName: string) {
+    ProblemApi.deleteTestData(
+        this.problem!.problemId!,
+        fileName,
+        this.userInfo
+    ).then(() => {
+      this.message.warning(`${fileName} 已删除`)
+    }).catch((error: ErrorMsg) => {
+      this.message.error(error.toString())
+    }).finally(() => {
+      this.queryData(this.problem!.problemId!)
+    })
+  }
 }
 </script>
 
-<style scoped lang="scss">
+<style scoped>
 .test-data-div {
   height: calc(100% - var(--layout-padding));
   width: calc(100% - var(--layout-padding) * 2);
   padding: var(--layout-padding);
   display: flex;
   flex-direction: column;
+}
+</style>
+
+<style lang="scss">
+.test-data-div {
+  .n-upload-trigger {
+    width: 100%;
+  }
 }
 </style>
