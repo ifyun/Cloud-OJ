6ffcc7b6ba65bb97f93c663146405b5e2598d874
Cloud <wfloatingcloud@outlook.com>
2022-07-29 15:34:24 +0800

    style: remove unused code

--- gateway/src/main/java/group/_204/oj/gateway/model/User.java
+++ gateway/src/main/java/group/_204/oj/gateway/model/User.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
