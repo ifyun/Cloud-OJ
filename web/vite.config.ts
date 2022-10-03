368ccd4953f8bc388781ff26bb7be56239aa1516
Cloud <wfloatingcloud@outlook.com>
2022-10-03 19:59:22 +0800

    build: add gzip for vite

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,10 +3,17 @@ import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import vueJsx from "@vitejs/plugin-vue-jsx"
+import viteCompression from "vite-plugin-compression"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    viteCompression({
+      threshold: 20480
+    })
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
