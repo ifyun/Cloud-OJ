782458e7d70b30a5f70f9533159f84888c053d1d
Cloud <wfloatingcloud@outlook.com>
2023-01-25 15:45:21 +0800

    build: update build script and config file

---
+++ services/judge/src/main/java/cloud/oj/judge/JudgeApp.java
@@ -0,0 +1,34 @@
+package cloud.oj.judge;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.SystemUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Slf4j
+@EnableFeignClients
+@EnableTransactionManagement
+@SpringBootApplication
+public class JudgeApp {
+    private static void checkSystem() {
+        if (!SystemUtils.IS_OS_LINUX) {
+            log.error("Only support Linux.");
+            System.exit(1);
+        }
+
+        var arch = System.getProperty("os.arch");
+        log.info("OS Arch: {}", arch);
+
+        if (!arch.equals("amd64")) {
+            log.error("Only support x64.");
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args) {
+        checkSystem();
+        SpringApplication.run(JudgeApp.class, args);
+    }
+}
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue"
+import axios from "axios"
 import router from "@/router"
 import store from "@/store"
 import "vfonts/Inter.css"
@@ -6,4 +7,5 @@ import "vfonts/FiraCode.css"
 import App from "./App.vue"
 import "./style.scss"
 
+axios.defaults.validateStatus = () => true
 createApp(App).use(router).use(store).mount("#app")
