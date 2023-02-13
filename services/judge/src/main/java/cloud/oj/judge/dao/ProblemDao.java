c63eb79a65454ff512c3d2f6dfa5018f63e31c17
Cloud <wfloatingcloud@outlook.com>
2023-02-13 15:28:32 +0800

    fix: leaderboard api
    fix leaderboard api
    remove unused code

--- services/judge/src/main/java/cloud/oj/judge/dao/ProblemDao.java
+++ services/judge/src/main/java/cloud/oj/judge/dao/ProblemDao.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface ProblemDao {
     Limit getLimit(int problemId);
-    String getSql(int problemId);
 
     boolean isOpen(int problemId);
 }
